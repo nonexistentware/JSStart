5badd32ecc991bd8fe96c6b3cba815a948dcf0a0
nonexistentware <nonexistentware@gmail.com>
2025-01-29 21:55:04 +0200

    removed var, changed to let

--- HomeWork9.1/main.js
+++ HomeWork9.1/main.js
@@ -1,5 +1,5 @@
 function changeText(elementId, newText) {
-    var element = document.getElementById(elementId);
+    let element = document.getElementById(elementId);
     if (element) {
         element.textContent = newText;
     } else {
